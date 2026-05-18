3279498f59ee287f4593dc8587c068977c201672
Ariel Guevara <adguevara7@espe.edu.ec>
2026-05-18 12:27:44 -0500

    cambios en cliente consola

--- 04.CLICON/CliUniversalConsole/CliUniversalConsole/Config/ServiceConfig.cs
+++ 04.CLICON/CliUniversalConsole/CliUniversalConsole/Config/ServiceConfig.cs
@@ -3,7 +3,7 @@ namespace CliUniversalConsole.Config
     public static class ServiceConfig
     {
         // URLs para servicios REST - Autenticación
-        public static string RestJavaBaseUrl { get; set; } = "http://localhost:8080/EurekaBank_RestFull_Java_GR05/api/Autenticacion";
+        public static string RestJavaBaseUrl { get; set; } = "http://localhost:8081/EurekaBank_RestFull_Java_GR05/api/Autenticacion";
         public static string RestDotNetBaseUrl { get; set; } = "http://localhost:9099/api/Autenticacion";
 
         // URLs para servicios SOAP - Autenticación
@@ -11,7 +11,7 @@ namespace CliUniversalConsole.Config
         public static string SoapDotNetBaseUrl { get; set; } = "http://localhost:9098/ws/ServicioAutenticacion.svc";
 
         // URLs para servicios REST - Transacciones
-        public static string RestJavaTransaccionUrl { get; set; } = "http://localhost:8080/EurekaBank_RestFull_Java_GR05/api/Transaccion";
+        public static string RestJavaTransaccionUrl { get; set; } = "http://localhost:8081/EurekaBank_RestFull_Java_GR05/api/Transaccion";
         public static string RestDotNetTransaccionUrl { get; set; } = "http://localhost:9099/api/Transaccion";
 
         // URLs para servicios SOAP - Transacciones
@@ -19,7 +19,7 @@ namespace CliUniversalConsole.Config
         public static string SoapDotNetTransaccionUrl { get; set; } = "http://localhost:9098/ws/ServicioTransaccion.svc";
 
         // URLs para servicios REST - Reportes
-        public static string RestJavaReporteUrl { get; set; } = "http://localhost:8080/EurekaBank_RestFull_Java_GR05/api/Reporte";
+        public static string RestJavaReporteUrl { get; set; } = "http://localhost:8081/EurekaBank_RestFull_Java_GR05/api/Reporte";
         public static string RestDotNetReporteUrl { get; set; } = "http://localhost:9099/api/Reporte";
 
         // URLs para servicios SOAP - Reportes
